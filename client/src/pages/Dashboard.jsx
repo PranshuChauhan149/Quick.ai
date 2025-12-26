0c49f5402c329d4a13fb54a49e4f9bfcb8ba0662
PranshuChauhan149 <pranshuchauhan149@gmail.com>
2025-12-26 22:55:42 +0530

    done write article page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyCreationData } from "../../assets/assets";
 import { Gem, Sparkles } from "lucide-react";
 import { Protect } from "@clerk/clerk-react";
+import CreationItem from "../components/CreationItem";
 
 const Dashboard = () => {
   const [creations, setCreations] = useState([]);
@@ -12,7 +13,7 @@ const Dashboard = () => {
 
   return (
     <div className="h-full overflow-y-scroll p-6">
-      <div className="flex w-full justify-start gap-4 flex-wrap">
+      <div className="flex justify-start gap-4 flex-wrap">
         <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
           <div className="text-slate-600">
             <p className="text-sm">Total Creations</p>
@@ -22,19 +23,28 @@ const Dashboard = () => {
             <Sparkles className="w-5 h-5" />
           </div>
         </div>
-      </div>
-
-
-      <div className="flex justify-start gap-4 flex-wrap">
-        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
-          <div className="text-slate-600">
-            <p className="text-sm">Active Plan</p>
-            <h2 className="text-xl font-semibold"><Protect plan='premium' fallback="Free" >Premium</Protect></h2>
-          </div>
-          <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
-            <Gem className="w-5 h-5" />
+        <div className="flex justify-start gap-4 flex-wrap">
+          <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+            <div className="text-slate-600">
+              <p className="text-sm">Active Plan</p>
+              <h2 className="text-xl font-semibold">
+                <Protect plan="premium" fallback="Free">
+                  Premium
+                </Protect>
+              </h2>
+            </div>
+            <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5] to-[#9E53EE] text-white flex justify-center items-center">
+              <Gem className="w-5 h-5" />
+            </div>
           </div>
         </div>
+      </div>
+
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {creations.map((item) => (
+          <CreationItem item={item} key={item.id} />
+        ))}
       </div>
     </div>
   );
